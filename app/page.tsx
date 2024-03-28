a78ccae4e174a3155e7ed1e378fa27f93c2967a9
zntb <zenetibi@yahoo.com>
2024-03-28 12:48:19 +0200

    Add Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from '@/components/ui/button';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <Button variant="outline" size="lg">
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
   );
 }
