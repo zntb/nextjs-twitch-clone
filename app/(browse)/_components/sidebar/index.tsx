bb1df9bad7a202482acbdf4f023c103af4e03171
zntb <zenetibi@yahoo.com>
2024-03-31 10:50:32 +0300

    Add Sidebar

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { Toggle } from './toggle';
+import { Wrapper } from './wrapper';
+
+export const Sidebar = () => {
+  return (
+    <Wrapper>
+      <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0"></div>
+    </Wrapper>
+  );
+};
