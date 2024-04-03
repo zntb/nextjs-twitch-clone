66585cc4b318b839175704f0b81ccb4de9a55a64
zntb <zenetibi@yahoo.com>
2024-04-03 17:09:23 +0300

    Add Community component

--- components/stream-player/chat.tsx
+++ components/stream-player/chat.tsx
@@ -14,6 +14,7 @@ import { ChatVariant, useChatSidebar } from '@/store/use-chat-sidebar';
 import { ChatForm, ChatFormSkeleton } from './chat-form';
 import { ChatList, ChatListSkeleton } from './chat-list';
 import { ChatHeader, ChatHeaderSkeleton } from './chat-header';
+import { ChatCommunity } from './chat-community';
 
 interface ChatProps {
   hostName: string;
@@ -83,6 +84,13 @@ export const Chat = ({
             isFollowing={isFollowing}
           />
         </>
+      )}
+      {variant === ChatVariant.COMMUNITY && (
+        <ChatCommunity
+          viewerName={viewerName}
+          hostName={hostName}
+          isHidden={isHidden}
+        />
       )}
     </div>
   );
