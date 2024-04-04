121ec6989a42f6a5ccd295d4560eb20f137ef490
zntb <zenetibi@yahoo.com>
2024-04-04 13:50:52 +0300

    Add  Info card

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from '@clerk/nextjs';
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks(.*)'],
+  publicRoutes: ['/', '/api/webhooks(.*)', '/api/uploadthing'],
 });
 
 export const config = {
