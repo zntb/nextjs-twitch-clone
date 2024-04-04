5d9704829da4ad96db79f93cfd931203c212edea
zntb <zenetibi@yahoo.com>
2024-04-04 18:23:27 +0300

    Add User page not-found and error pages

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,18 @@
 import { authMiddleware } from '@clerk/nextjs';
 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks(.*)', '/api/uploadthing'],
+  publicRoutes: [
+    '/',
+    '/api/webhooks(.*)',
+    '/api/uploadthing',
+    '/:username',
+    '/search',
+  ],
 });
 
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    '/((?!.+\\.[\\w]+$|_next).*)',
-    // Re-include any files in the api or trpc folders that might have an extension
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
